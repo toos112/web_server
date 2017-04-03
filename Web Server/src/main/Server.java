4a3d74dbfcc788ff6d693ba1819300e2688a4bc3
toos112 <toos112kp@gmail.com>
2017-04-03 15:30:00 +0200

    test 236343

--- Web Server/src/main/Server.java
+++ Web Server/src/main/Server.java
@@ -2,13 +2,8 @@ package main;
 
 public class Server {
 	
-	public static void test2() {
-		
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-		test2();
 	}
 
 }
