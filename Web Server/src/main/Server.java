57d20786efb2a7698095688aa365f1facdd52836
DutChen18 <DutChen18@supercomputer>
2017-04-03 15:24:59 +0200

    test 378287

--- Web Server/src/main/Server.java
+++ Web Server/src/main/Server.java
@@ -2,15 +2,8 @@ package main;
 
 public class Server {
 	
-	public static void test2() {
-		System.out.println("Hello, Chen!");
-		System.out.println("Does it work?");
-		System.out.println("Next");
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-		test2();
 	}
 
 }
