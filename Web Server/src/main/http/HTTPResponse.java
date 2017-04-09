b59be936e0045c7d66c81d3d611cb87a496e57b6
DutChen18 <DutChen18@supercomputer>
2017-04-09 13:27:28 +0200

    added HTTPResponse class

---
+++ Web Server/src/main/http/HTTPResponse.java
@@ -0,0 +1,26 @@
+package main.http;
+
+import java.util.Hashtable;
+
+public class HTTPResponse {
+
+	private Hashtable<String, String> header = new Hashtable<String, String>();
+	private String body;
+	
+	public HTTPResponse() {
+		
+	}
+	
+	String getText() {
+		return "";
+	}
+	
+	void addHeader(String key, String value) {
+		
+	}
+	
+	void setBody(String body) {
+		
+	}
+
+}
