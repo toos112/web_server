4375a2996bcab33ca860fe31974e7a9a67f0de96
DutChen18 <DutChen18@supercomputer>
2017-04-09 12:33:38 +0200

    remove test files

--- Web Server/src/main/Server.java
+++ Web Server/src/main/Server.java
@@ -2,13 +2,8 @@ package main;
 
 public class Server {
 	
-	public static void test() {
-		
-	}
-	
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		test();
+		
 	}
 
 }
--- Web Server/src/main/Test.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Test {
-	
-	Test() {
-		
-	}
-
-}
