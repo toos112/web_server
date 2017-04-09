d41d1b71a8f3c39dba7ed5ede4737ff0a86b09fa
toos112 <toos112kp@gmail.com>
2017-04-09 14:05:46 +0200

    Filefinish

--- Web Server/src/main/io/File.java
+++ Web Server/src/main/io/File.java
@@ -1,5 +1,26 @@
 package main.io;
 
-public class File {
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class File {
+	
+	private String filePath;
+	
+	public File(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	public String[] getContents() {
+		Path path = FileSystems.getDefault().getPath(filePath);
+		try {
+			return (String[]) Files.lines(path).toArray();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 }
