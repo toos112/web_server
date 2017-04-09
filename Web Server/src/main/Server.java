5443b8bd2bd5a772edf9b22c0fb1e7e017f07bfd
DutChen18 <DutChen18@supercomputer>
2017-04-09 13:25:35 +0200

    HTTPRequest completed

--- Web Server/src/main/Server.java
+++ Web Server/src/main/Server.java
@@ -1,8 +1,13 @@
 package main;
 
+import main.http.HTTPRequest;
+
 public class Server {
-	
+
 	public static void main(String[] args) {
+		
+		HTTPRequest req = new HTTPRequest("GET / HTTP/1.1\r\nHost: localhost\r\nUser-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: nl,en-US;q=0.7,en;q=0.3\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\n\r\n");
+		System.out.println(req.getValue("Host"));
 		
 	}
 
