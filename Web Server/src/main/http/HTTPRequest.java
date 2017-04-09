78f49f461b3809f725e742ac585286bead8bf52d
DutChen18 <DutChen18@supercomputer>
2017-04-09 13:47:35 +0200

    update HTTPRequest

--- Web Server/src/main/http/HTTPRequest.java
+++ Web Server/src/main/http/HTTPRequest.java
@@ -1,24 +1,33 @@
 package main.http;
 
-import java.util.Hashtable;
+import java.util.HashMap;
 
 public class HTTPRequest {
 
-	private Hashtable<String, String> header = new Hashtable<String, String>();
+	private HashMap<String, String> header = new HashMap<String, String>();
+	private String status;
 
 	public HTTPRequest(String str) {
 		String[] lines = str.split("\r\n");
 		for (int i = 0; i < lines.length; i++) {
-			if (i == 0) continue;
 			String line = lines[i];
-			if (line.equals("")) continue;
+			if (i == 0) {
+				status = line;
+				continue;
+			}
+			if (line.equals(""))
+				continue;
 			String[] pair = line.split(": ");
 			header.put(pair[0], pair[1]);
 		}
 	}
-	
+
 	public String getValue(String key) {
 		return header.get(key);
+	}
+	
+	public String getStatus() {
+		return status;
 	}
 
 }
--- Web Server/src/main/http/HTTPResponse.java
+++ Web Server/src/main/http/HTTPResponse.java
@@ -1,22 +1,26 @@
 package main.http;
 
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HTTPResponse {
 
-	private Hashtable<String, String> header = new Hashtable<String, String>();
+	private HashMap<String, String> header = new HashMap<String, String>();
 	private String body;
 	private String status;
-	
+
 	public HTTPResponse(String status, String body) {
 		this.status = status;
 		this.body = body;
 	}
-	
+
 	String getText() {
-		return status + "\n" + header;
+		String headers = "";
+		for (Map.Entry<String, String> entry : header.entrySet())
+			headers += entry.getKey() + ": " + entry.getValue() + "\n";
+		return status + "\n" + headers + "\n" + body;
 	}
-	
+
 	void addHeader(String key, String value) {
 		header.put(key, value);
 	}
