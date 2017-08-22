ad152e8f1f3c8c1de8b9c95e23c72e978f99fc2b
DutChen18 <DutChen18@gmail.com>
2017-08-22 13:54:41 +0200

    command system activate!

--- Web Server/src/main/Server.java
+++ Web Server/src/main/Server.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import main.http.HTTPSServer;
 import main.http.HTTPServer;
+import main.util.CommandHandler;
 import main.util.js.ServerScriptManager;
 
 public class Server {
@@ -13,13 +14,14 @@ public class Server {
 			int httpPort = Integer.parseInt(args[0]);
 			HTTPServer http = new HTTPServer(httpPort, args.length >= 2);
 			new Thread(ServerScriptManager.instance).run();
-			System.out.println("Created server!");
 			new Thread(http).start();
 			if (args.length >= 2) {
 				int httpsPort = Integer.parseInt(args[1]);
 				HTTPSServer https = new HTTPSServer(httpsPort);
 				new Thread(https).start();
 			}
+			System.out.println("Created server!");
+			new Thread(new CommandHandler()).run();
 		} catch (IOException e) {
 			System.err.println("Failed to create server!");
 			e.printStackTrace();
